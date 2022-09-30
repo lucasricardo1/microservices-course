deb9bae369c5d974c49e7dbbb5e79b60ce1cf2ea
lucasricardo1 <lucasricardox.o@gmail.com>
2022-09-30 12:01:36 -0300

    Project hr-user created

---
+++ hr-user/src/test/java/com/lucasricardo/hruser/HrUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucasricardo.hruser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
