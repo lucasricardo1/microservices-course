eef289ea081a38f2251607b6fe0fba3a4055ee95
lucasricardo1 <lucasricardox.o@gmail.com>
2022-09-30 10:28:47 -0300

    hr-worker as a config server client

--- hr-worker/src/main/java/com/lucasricardo/hrworker/resources/WorkerResource.java
+++ hr-worker/src/main/java/com/lucasricardo/hrworker/resources/WorkerResource.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.hibernate.annotations.common.util.impl.LoggerFactory;
 import org.slf4j.*;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -21,11 +22,21 @@ public class WorkerResource {
 	
 	private static Logger logger = org.slf4j.LoggerFactory.getLogger(WorkerResource.class);
 	
+	@Value("${test.config}")
+	private String testConfig;
+	
 	@Autowired
 	private Environment env;
 
 	@Autowired
 	private WorkerRepository repository;
+	
+	@GetMapping(value = "/configs")
+	public ResponseEntity<Void> getConfigs(){
+		logger.info("CONFIG = " + testConfig);
+		
+		return ResponseEntity.noContent().build();
+	}
 
 	@GetMapping
 	public ResponseEntity<List<Worker>> findAll(){
