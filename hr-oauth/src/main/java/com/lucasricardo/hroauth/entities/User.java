227d23f28785ba6851554759b3e6c79a91ee334a
lucasricardo1 <lucasricardox.o@gmail.com>
2022-09-30 18:11:16 -0300

    Login, JWT token

--- hr-oauth/src/main/java/com/lucasricardo/hroauth/entities/User.java
+++ hr-oauth/src/main/java/com/lucasricardo/hroauth/entities/User.java
@@ -1,11 +1,17 @@
 package com.lucasricardo.hroauth.entities;
 
+import java.util.Collection;
 import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
+import java.util.stream.Collectors;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 
-public class User {
+public class User implements UserDetails{
 
 	private Long id;
 	private String name;
@@ -77,6 +83,39 @@ public class User {
 			return false;
 		User other = (User) obj;
 		return Objects.equals(id, other.id);
+	}
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		return roles
+				.stream()
+				.map(x -> new SimpleGrantedAuthority(x.getRoleName()))
+				.collect(Collectors.toList());
+	}
+
+	@Override
+	public String getUsername() {
+		return email;
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		return true;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		return true;
 	}
 	
 }
